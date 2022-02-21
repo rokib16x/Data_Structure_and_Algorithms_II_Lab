5a577c05f5989b73ccec650e984cc731b972ea19
Rokibul Hasan <86328558+rokib16x@users.noreply.github.com>
2022-02-21 22:35:27 +0600

    Create problem_2.cpp

---
+++ Class 1/problem_2.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    char input;
+    while (true){
+        cout<<"Please Enter: ";
+        cin>>input;
+        if(input=='A') {
+            cout<<"Accepted";
+            break;
+        } else
+            cout<<"Please try again"<<endl;
+        cin.seekg(0,ios::end);
+        cin.clear();
+    }
+    return 0;
+
+}
