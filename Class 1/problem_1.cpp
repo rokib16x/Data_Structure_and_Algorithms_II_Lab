b875f4455a3c9c38b5d6b1b01e97905ee9d04eb1
Rokibul Hasan <86328558+rokib16x@users.noreply.github.com>
2022-02-21 22:26:14 +0600

    Create problem_1.cpp

---
+++ Class 1/problem_1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int x,n;
+    double sum = 0;
+    cout<<"Your input capacity: ";
+    cin>>n;
+    for (int i = 0; i < n; ++i) {
+        cout<<"Number no-"<<i+1<<": ";
+        cin>>x;
+        sum+=x;
+    }
+    cout<<"Average: "<<fixed<<setprecision(2)<<sum/n;
+    return 0;
+
+}
