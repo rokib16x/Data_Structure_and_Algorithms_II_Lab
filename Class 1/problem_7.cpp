7a24bb309ed5793800fd1c75ac446f3a1dda57bb
Rokibul Hasan <86328558+rokib16x@users.noreply.github.com>
2022-02-23 13:42:47 +0600

    Update problem_7.cpp

--- Class 1/problem_7.cpp
+++ Class 1/problem_7.cpp
@@ -7,58 +7,59 @@ int main(){
     getline(cin,s);
     unsigned long int n = s.length()+1;
     for (int i = 0; i < n; ++i) {
-        if(s[i] == 'A')
+                if(s[i] == 'A')
             s[i]='a';
-        if(s[i] == 'B')
+        else if(s[i] == 'B')
             s[i]='b';
-        if(s[i] == 'C')
+        else if(s[i] == 'C')
             s[i]='c';
-        if(s[i] == 'D')
+        else if(s[i] == 'D')
             s[i]='d';
-        if(s[i] == 'E')
+        else if(s[i] == 'E')
             s[i]='e';
-        if(s[i] == 'F')
+        else if(s[i] == 'F')
             s[i]='f';
-        if(s[i] == 'G')
+        else if(s[i] == 'G')
             s[i]='G';
-        if(s[i] == 'H')
+        else if(s[i] == 'H')
             s[i]='h';
-        if(s[i] == 'I')
+        else if(s[i] == 'I')
             s[i]='i';
-        if(s[i] == 'J')
+        else if(s[i] == 'J')
             s[i]='j';
-        if(s[i] == 'K')
+        else if(s[i] == 'K')
             s[i]='k';
-        if(s[i] == 'L')
+        else if(s[i] == 'L')
             s[i]='l';
-        if(s[i] == 'M')
+        else if(s[i] == 'M')
             s[i]='m';
-        if(s[i] == 'N')
+        else if(s[i] == 'N')
             s[i]='n';
-        if(s[i] == 'O')
+        else if(s[i] == 'O')
             s[i]='o';
-        if(s[i] == 'P')
+        else if(s[i] == 'P')
             s[i]='p';
-        if(s[i] == 'Q')
+        else if(s[i] == 'Q')
             s[i]='q';
-        if(s[i] == 'R')
+        else if(s[i] == 'R')
             s[i]='r';
-        if(s[i] == 'S')
+        else if(s[i] == 'S')
             s[i]='s';
-        if(s[i] == 'T')
+        else if(s[i] == 'T')
             s[i]='t';
-        if(s[i] == 'U')
+        else if(s[i] == 'U')
             s[i]='u';
-        if(s[i] == 'V')
+        else if(s[i] == 'V')
             s[i]='v';
-        if(s[i] == 'W')
+        else if(s[i] == 'W')
             s[i]='w';
-        if(s[i] == 'X')
+        else if(s[i] == 'X')
             s[i]='x';
-        if(s[i] == 'Y')
+        else if(s[i] == 'Y')
             s[i]='y';
-        if(s[i] == 'Z')
+        else if(s[i] == 'Z')
             s[i]='z';
+            else s[i]=s[i];
     }
 
     for (int i = 0; i < n; ++i) {
